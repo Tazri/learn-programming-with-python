ec2eee3678f2e942ee0036e98d17792ce979068f
Tazri <mdtazri@gmail.com>
2022-08-18 23:32:14 +0600

    bst_delte function implement done

--- part_3/chapter_10/binary_search_tree.py
+++ part_3/chapter_10/binary_search_tree.py
@@ -44,8 +44,16 @@ def search_bst(_root,_key):
 
     return current_node;
 
+# minimum
+def bst_minimun(_root:Binary_Tree.Node)->Binary_Tree.Node:
+    left = _root;
+
+    while left.left : left = left.left;
+
+    return left;
+
 # transplant
-def transplant(_root:Binary_Tree.Node,_current_node:Binary_Tree.Node,_new_node:Binary_Tree.Node):
+def bst_transplant(_root:Binary_Tree.Node,_current_node:Binary_Tree.Node,_new_node:Binary_Tree.Node):
     if _current_node.parent == None :
         _root = _new_node;
     elif _current_node == _current_node.parent.left :
@@ -56,6 +64,44 @@ def transplant(_root:Binary_Tree.Node,_current_node:Binary_Tree.Node,_new_node:B
     return _root;
 
 
+# delete
+def bst_delete(_root:Binary_Tree.Node,_node:Binary_Tree.Node):
+    # if left dose not exist or both dose not exist
+    if _node.left == None :
+        bst_transplant(_root,_node,_node.right);
+        return _root;
+    
+    # if right dose not exist
+    if _node.right == None:
+        bst_transplant(_root,_node,_node.left);
+        return _root;
+
+    # if left and right both exist 
+    # find the successor
+    successor = bst_minimun(_node.right);
+
+    # if node was parent
+    if _node.parent == None :
+        p_left = _root.left;
+        p_right = _root.right;
+
+        bst_transplant(_root,successor,successor.right);
+        bst_transplant(_root,_node,successor);
+        successor.add_left(p_left);
+        successor.add_right(p_right);      
+        return successor;
+
+    # if node was not parent
+    node_parent = _node.parent;
+    n_left = _node.left;
+    n_right = _node.right;
+    bst_transplant(_root,successor,successor.right);
+    bst_transplant(_root,_node,successor);
+    successor.add_left(n_left);
+    successor.add_right(n_right);
+    successor.parent = node_parent;
+    return _root;
+
 # create on kind of bst
 def create_bst():
     """
@@ -64,8 +110,8 @@ def create_bst():
               5     17
              / \   /  \
             3   7 12  19
-           / \
-          1   4
+           / \      \
+          1   4     13
     """
 
     
@@ -106,3 +152,48 @@ if __name__ == '__main__':
     print('\n\n> Search 8 in bst : ');
     node_eight = search_bst(root,8);
     print(node_eight);
+
+
+    print("\n\n>>> check bst_minimum <<<");
+    print("bst_minimum(root) : ",bst_minimun(root));
+    print("bst_minimum(root.right) : ",bst_minimun(root.right));
+    print("bst.minimum(root.left) : ",bst_minimun(root.left));
+
+    print("\n\n>>> check bst_delete <<<");
+    delete_result = bst_delete(root,root);
+
+    """
+                 12 
+               /    \
+              5     17
+             / \   /  \
+            3   7 13  19
+           / \      
+          1   4     
+    """
+
+
+    root = delete_result;
+    print("After bst_delete(root,root) : ");
+    print(delete_result);
+    print(delete_result.left);
+    print(delete_result.right);
+    print(delete_result.right.right);
+
+
+    delete_result = bst_delete(root,node_five);
+    print("\n>>> After bst_delete(root,node_five) <<<");
+    """
+             12 
+           /    \
+          7     17
+         /     /  \
+        3     13  19
+       / \      
+      1   4     
+    """
+    print(root);
+    print(root.left);
+    print(root.left.left);
+    print(root.left.left.left);
+    print(root.left.left.right);
