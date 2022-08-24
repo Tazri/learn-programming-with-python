29b78eea646d0e4aa8d4d42015a6ab47fdc31b87
Tazri <mdtazri@gmail.com>
2022-08-24 23:15:59 +0600

    implement the max_heapify wrong way.

--- part_3/chapter_11/heap.py
+++ part_3/chapter_11/heap.py
@@ -33,6 +33,9 @@ def create_heap()->list:
 
     return heap;
 
+# create another heap list
+def create_heap_list()->list: return [None,12,7,1,3,10,17,19,2,5];
+
 # left child
 def get_left(_i:int)-> int : return _i *2;
 
@@ -53,6 +56,37 @@ def is_max_heap(_heap):
     
     return True;
 
+# swap list value
+def swap(_list,_ai,_bi)-> list :
+    temp = _list[_ai];
+    _list[_ai] = _list[_bi];
+    _list[_bi] = temp;
+    return _list;
+
+# max_heapify
+def max_heapify(_heap,_root)-> list :
+    last_index = len(_heap) - 1;
+    li = get_left(_root); # left index
+    ri = get_right(_root);
+    l = _heap[li]; # left
+    r = _heap[ri]; # right
+    rt = _heap[_root]; # root 
+
+    # if root, left or right is equal or greater than last index
+    if li >= last_index or ri >= last_index or _root >= last_index :
+        return _heap;
+
+    # if is _root less than left or right
+    if rt < l or rt < r :
+        if l > r : # swaping
+            swap(_heap,li,_root);
+            max_heapify(_heap,li);
+        else :
+            swap(_heap,ri,_root);
+            max_heapify(_heap,ri);
+    
+    max_heapify(_heap,_root+1);
+
 if __name__ == '__main__' :
     heap = create_heap();
 
@@ -60,3 +94,16 @@ if __name__ == '__main__' :
     print(build(heap[1:]));
 
     print('is_max_heap(heap) : ',is_max_heap(heap));
+
+    # before using max_hapify
+    heap_two = create_heap_list();
+    print("\n\n>>> Before Max Heapifpy heap two <<<");
+    print(build(heap_two[1:]));
+    print("is_max_heap(heap_two) : ",is_max_heap(heap_two));
+
+    # after using max_hapify
+    print("\n>>> After Max Heapifoy heap two <<<");
+    max_heapify(heap_two,1);
+    print(build(heap_two[1:]));
+    print("is_max_heap(heap_two) : ",is_max_heap(heap_two));
+    
