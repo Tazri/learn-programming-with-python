bde82c837cdc00a79fcef396dccb6207bca40fc1
Tazri <mdtazri@gmail.com>
2023-01-04 23:53:05 +0600

    write basic function file

---
+++ part_4/chapter_12/basic_function.py
@@ -0,0 +1,33 @@
+import time;
+
+def please_sleep(sec=1.1):
+    print("Sleeping........ for 1.1 sec");
+    time.sleep(sec);
+
+def compute_fnc():
+    print("Computing..........");
+    for x in range(1000000):
+        _ = pow(x,123);
+
+for func in [please_sleep,compute_fnc]:
+    print(f">>>> {func.__name__} :");
+
+    t1 = time.perf_counter();
+    func();
+    print("after calling finish :")
+    t2 = time.perf_counter();
+    time_spent = t2 - t1;
+    print(f"time spent : {round(time_spent,2)}s");
+    print();
+
+'''
+>>>> please_sleep :
+Sleeping........ for 1.1 sec
+after calling finish :
+time spent : 1.1s
+
+>>>> compute_fnc :
+Computing..........
+after calling finish :
+time spent : 3.47s
+'''
